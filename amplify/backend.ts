4df081845edbdc120626cfeb085e493963663500
Harshita Daddala <harshita.d11@gmail.com>
2024-08-14 16:31:26 -0400

    backend changes

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 // import { data } from './data/resource';
-import { firstbucket,secondbucket } from './storage/resource';
+import { firstbucket } from './storage/resource';
 
 defineBackend({
   auth,
   // data,
-  firstbucket,
-  secondbucket
+  firstbucket
 });
