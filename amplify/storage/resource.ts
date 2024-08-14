14b7277e38c1c6f88bc20eb7ac390893c68b41f8
Harshita Daddala <harshita.d11@gmail.com>
2024-08-14 19:54:03 -0400

    change default bucket

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,7 +2,6 @@ import { defineStorage } from '@aws-amplify/backend';
 
 export const firstbucket = defineStorage({
   name: 'mybucket1',
-  isDefault: true,
   access: (allow) => ({
     'photos/*': [
       allow.guest.to(['read','write']),
@@ -12,6 +11,7 @@ export const firstbucket = defineStorage({
 
 export const secondbucket = defineStorage(
   {
-    name: "mybucket2"
+    name: "mybucket2",
+    isDefault: true,
   }
 );
